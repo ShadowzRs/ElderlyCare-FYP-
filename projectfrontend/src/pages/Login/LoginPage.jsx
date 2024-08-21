5d016a13ee56575174693a3e66e085a149bd11b7
ShadowzRs <140068095+ShadowzRs@users.noreply.github.com>
2024-08-22 05:23:03 +0800

    Updated Landing and Register

--- projectfrontend/src/pages/Login/LoginPage.jsx
+++ projectfrontend/src/pages/Login/LoginPage.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-import Background from "../../assets/Background-Box.svg";
-import logo from "../../assets/AI-LifeConnect_Logo.png";
 import Header_v2 from "../../component/Header/Header-v2.jsx";
 import "./LoginPage.css";
 
