d9e075867ca7ba31ca734ba327fea6d95d595886
ShadowzRs <140068095+ShadowzRs@users.noreply.github.com>
2024-09-19 10:13:51 +0800

    AI-Chat endpoint added

---
+++ src/main/java/com/LifeConnect/ElderlyCareSystem/controller/AIChatController.java
@@ -0,0 +1,19 @@
+package com.LifeConnect.ElderlyCareSystem.controller;
+
+import com.LifeConnect.ElderlyCareSystem.service.AIChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class AIChatController {
+
+    @Autowired
+    private AIChatService aiService;
+
+    @GetMapping("/Ollama/generate")
+    public String generate(@RequestParam(value = "promptMessage") String promptMessage) {
+        return aiService.generateResult(promptMessage);
+    }
+}
